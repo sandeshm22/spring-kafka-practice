23c354bd8fe5d7234572284d13597ea0baf3d6cc
Sandesh Mutha <aadeshmutha@gmail.com>
2024-02-04 19:48:59 +0530

    Test

--- src/main/java/com/practice/springkafkapractice/KafkaProducerConsumer.java
+++ src/main/java/com/practice/springkafkapractice/KafkaProducerConsumer.java
@@ -25,25 +25,15 @@ public class KafkaProducerConsumer {
         sendData();
         sendData();
         sendData();
-        sendData();
-        sendData();
-        sendData();
-        sendData();
-        sendData();
-        sendData();
-        sendData();
-        sendData();
-        sendData();
-
     }
 
     private void sendData() {
         String correlationId = UUID.randomUUID().toString();
-        ProducerRecord<String, String> requestFile = new ProducerRecord<>("request", correlationId,  data[0]);
-        ProducerRecord<String, String> requestFile1= new ProducerRecord<>("request", UUID.randomUUID().toString(), data[1]);
-        ProducerRecord<String, String> requestFile2 = new ProducerRecord<>("request", UUID.randomUUID().toString(), data[2]);
-        ProducerRecord<String, String> requestFile3 = new ProducerRecord<>("request", UUID.randomUUID().toString(), data[3]);
-        ProducerRecord<String, String> requestFile4 = new ProducerRecord<>("request", UUID.randomUUID().toString(), data[4]);
+        ProducerRecord<String, String> requestFile = new ProducerRecord<>("request1", correlationId,  data[0]);
+        ProducerRecord<String, String> requestFile1= new ProducerRecord<>("request1", UUID.randomUUID().toString(), data[1]);
+        ProducerRecord<String, String> requestFile2 = new ProducerRecord<>("request1", UUID.randomUUID().toString(), data[2]);
+        ProducerRecord<String, String> requestFile3 = new ProducerRecord<>("request1", UUID.randomUUID().toString(), data[3]);
+        ProducerRecord<String, String> requestFile4 = new ProducerRecord<>("request1", UUID.randomUUID().toString(), data[4]);
         template.send(requestFile);
         template.send(requestFile1);
         template.send(requestFile2);
--- src/main/java/com/practice/springkafkapractice/processor/Handler.java
+++ src/main/java/com/practice/springkafkapractice/processor/Handler.java
@@ -1,8 +1,10 @@
-package com.practice.springkafkapractice;
+package com.practice.springkafkapractice.processor;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Named;
+import org.apache.kafka.streams.kstream.Suppressed;
 import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -10,6 +12,8 @@ import org.springframework.stereotype.Component;
 import java.time.Duration;
 import java.util.Map;
 import java.util.function.Consumer;
+
+import static org.apache.kafka.streams.kstream.Suppressed.BufferConfig.unbounded;
 
 @Component
 public class Handler {
@@ -24,15 +28,17 @@ public class Handler {
     public Consumer<KStream<String, String>> consumeMessage() {
         return (consumeMessageStream) -> consumeMessageStream
                 .groupBy((key, value) -> getValue(value))
-                .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofHours(1)))
-                .count().toStream()
+                //.windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofHours(1)))
+                .windowedBy(TimeWindows.of(Duration.ofMinutes(10)))
+                .count(Named.as("counts-store"))
+                .suppress(Suppressed.untilWindowCloses(unbounded()))
+                .toStream()
                 .foreach((k, v) -> System.out.printf("Key %s Count is - %s%n", k.key(), v));
     }
 
     private String getValue(String value) {
         try {
             Map<String, String> dropfile = mapper.readValue(value, Map.class);
-            //System.out.println(dropfile);
             return dropfile.get("Client");
         } catch (JsonProcessingException e) {
             return "";
