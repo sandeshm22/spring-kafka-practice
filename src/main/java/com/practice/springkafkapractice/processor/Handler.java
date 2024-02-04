11c69e540473d45db3637365d5c4a2f1b2cd7c11
Sandesh Mutha <aadeshmutha@gmail.com>
2024-02-04 19:51:34 +0530

    Test

--- src/main/java/com/practice/springkafkapractice/processor/Handler.java
+++ src/main/java/com/practice/springkafkapractice/processor/Handler.java
@@ -3,7 +3,6 @@ package com.practice.springkafkapractice.processor;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Named;
 import org.apache.kafka.streams.kstream.Suppressed;
 import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.context.annotation.Bean;
@@ -29,8 +28,8 @@ public class Handler {
         return (consumeMessageStream) -> consumeMessageStream
                 .groupBy((key, value) -> getValue(value))
                 //.windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofHours(1)))
-                .windowedBy(TimeWindows.of(Duration.ofMinutes(10)))
-                .count(Named.as("counts-store"))
+                .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofMinutes(10)))
+                .count()
                 .suppress(Suppressed.untilWindowCloses(unbounded()))
                 .toStream()
                 .foreach((k, v) -> System.out.printf("Key %s Count is - %s%n", k.key(), v));
@@ -38,8 +37,8 @@ public class Handler {
 
     private String getValue(String value) {
         try {
-            Map<String, String> dropfile = mapper.readValue(value, Map.class);
-            return dropfile.get("Client");
+            Map dropfile = mapper.readValue(value, Map.class);
+            return (String) dropfile.get("Client");
         } catch (JsonProcessingException e) {
             return "";
         }
